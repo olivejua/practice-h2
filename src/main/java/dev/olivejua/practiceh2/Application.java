1a7079fc60eafd156eb47af4f9c81aea417d3098
olivejua <tmfrl4710@gmail.com>
2021-02-17 16:34:41 +0900

    initial commit

--- src/main/java/dev/olivejua/practiceh2/Application.java
+++ src/main/java/dev/olivejua/practiceh2/Application.java
@@ -1,11 +1,28 @@
 package dev.olivejua.practiceh2;
 
+import dev.olivejua.practiceh2.domain.Item;
+import dev.olivejua.practiceh2.domain.ItemRepository;
+import dev.olivejua.practiceh2.domain.Shop;
+import dev.olivejua.practiceh2.domain.ShopRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    private ShopRepository shopRepository;
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        shopRepository.save(new Shop("olivejua", "olivejua-develop.tistory.com"));
+        itemRepository.save(new Item("olivejua's Github", 1000));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
