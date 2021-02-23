79cdce49719cd4320e8ce9217eab02a6d809d346
olivejua <tmfrl4710@gmail.com>
2021-02-23 22:17:23 +0900

    tcp server 설정

--- src/main/java/dev/olivejua/practiceh2/domain/H2ServerConfiguration.java
+++ src/main/java/dev/olivejua/practiceh2/domain/H2ServerConfiguration.java
@@ -1,10 +1,14 @@
 package dev.olivejua.practiceh2.domain;
 
+
+import com.zaxxer.hikari.HikariDataSource;
 import org.h2.tools.Server;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
 
 @Configuration
@@ -12,8 +16,9 @@ import java.sql.SQLException;
 public class H2ServerConfiguration {
 
     @Bean
-    public Server h2TcpServer() throws SQLException {
-        return Server.createTcpServer()
-                .start();
+    @ConfigurationProperties("spring.datasource.hikari") //yml의 설정값을 Set한다.
+    public DataSource h2TcpServer() throws SQLException {
+        Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092", "-ifNotExists").start();
+        return new HikariDataSource();
     }
 }
